364d8d4da51832d42376dbab173f4270673bb084
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-13 10:30:36 +0200

    add controller BookController

---
+++ src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.luv2code.springbootlibrary.controller;
+
+import com.luv2code.springbootlibrary.entity.Book;
+import com.luv2code.springbootlibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000") //allows frontend to access controller without CORS error
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com"; //update later
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
