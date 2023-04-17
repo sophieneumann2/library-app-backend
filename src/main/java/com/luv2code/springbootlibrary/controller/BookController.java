5dd9dadde8075d4826df5c3ee0ed00793ecbc345
Sophie Neumann <sophie.neumann@exxeta.com>
2023-04-17 11:43:22 +0200

    update book service and controller to include renewLoan functionality

--- src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
+++ src/main/java/com/luv2code/springbootlibrary/controller/BookController.java
@@ -45,10 +45,15 @@ public class BookController {
         return bookService.checkoutBook(userEmail, bookId);
     }
 
-
     @PutMapping("/secure/return")
     public void returnBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         bookService.returnBook(userEmail, bookId);
+    }
+
+    @PutMapping("/secure/renew/loan")
+    public void renewLoan(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.renewLoan(userEmail, bookId);
     }
 }
